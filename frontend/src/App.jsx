7e42159eeb688b645b1667a613c3b56283537222
Hemalatha267 <bandarihemalatha37@gmail.com>
2026-03-03 14:39:24 +0530

    Initial Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,28 @@
-import React from 'react'
-import User from './Components/User';
-import Parent from './components/Parent';
-import State from './hooks/State';   // ✅ Capital S
-import Form from './hooks/Form';   
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import ProductDetails from "./pages/ProductDetails";
+
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Services from "./pages/Services";
 
 const App = () => {
   return (
     <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/services/:id" element={<ProductDetails />} />
+      </Routes>
+
       <div>App</div>
-      <Form />
-      <State />          {/* ✅ Now matches import */}
-      <Parent />
-      <User 
-        name="xyz" 
-        age={19} 
-        skills={["HTML","CSS","JS","Java"]}
-      />
-    </>
-  );
-}
-
-export default App;
-
-
-
-/*import React from "react";
-import Parent from "./components/Parent";          // or "./components/Parent"
-import User from "./components/User";
-
-const App = () => {
-  return (
-    <>
-      <Parent />
-      <User name="xyz" age={33} skills={["HTML", "CSS", "JS", "JAVA"]} />
     </>
   );
 };
 
-export default App;*/
+export default App;
